a2652475fec55d2ac95407bda515156aa2923af9
Artori4z <kongphop.buch@bumail.net>
2024-10-22 22:17:53 +0700

    .

---
+++ Assets/scrip/GhostTwo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTwo : GhostAi
+{
+    protected override void GhostMove()
+    {
+        if (random <= difficult)
+        {
+            //if เดิน ไฟดับ
+            ghostTwoD.transform.position = ghostSpawnPoint[0].position;
+        }
+    }
+}
--- Assets/scrip/Ghostsiri.cs
+++ Assets/scrip/Ghostsiri.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class Ghostsiri : GhostAi
 {
-    private int _index = 1;
     private int _count = 0;
     private Quaternion _rotation;
     private int randomPosition;
+    private int positionNow;
     protected override void GhostMove()
     {
        if (random <= difficult)
@@ -16,48 +16,58 @@ public class Ghostsiri : GhostAi
             //spawn
             if (ghostTwoD.transform.position == startPosition.position) 
             {
+                randomPosition = Random.Range(1, 5);
                 ghostPosition(1);
+                positionNow = 1;
             }
         }
     }
     //move ghost 4 position
     protected void ghostPosition(int gPosition)
     {
-        switch(gPosition)
+        while (gPosition != positionNow) 
         {
-            case 1:
-                _rotation = Quaternion.Euler(0, 0, 180);
-                ghostTwoD.transform.rotation = _rotation;
-                ghostTwoD.transform.position = ghostSpawnPoint[0].position;
-                break;
-            case 2:
-                _rotation = Quaternion.Euler(0, -70, 180);
-                ghostTwoD.transform.rotation = _rotation;
-                ghostTwoD.transform.position = ghostSpawnPoint[1].position;
-                break;
-            case 3:
-                _rotation = Quaternion.Euler(0, 80, 180);
-                ghostTwoD.transform.rotation = _rotation;
-                ghostTwoD.transform.position = ghostSpawnPoint[2].position;
-                break;
-            case 4:
-                _rotation = Quaternion.Euler(0, 0, 180);
-                ghostTwoD.transform.rotation = _rotation;
-                ghostTwoD.transform.position = ghostSpawnPoint[3].position;
-                break;
+            gPosition = Random.Range(1, 5);
+            Debug.Log(gPosition);
         }
-        
+        //_count++;
+        if (_count < 2)
+        {
+            switch (gPosition)
+            {
+                case 1:
+                    _rotation = Quaternion.Euler(0, 0, 180);
+                    ghostTwoD.transform.rotation = _rotation;
+                    ghostTwoD.transform.position = ghostSpawnPoint[0].position;
+
+                    break;
+                case 2:
+                    _rotation = Quaternion.Euler(0, -70, 180);
+                    ghostTwoD.transform.rotation = _rotation;
+                    ghostTwoD.transform.position = ghostSpawnPoint[1].position;
+
+                    break;
+                case 3:
+                    _rotation = Quaternion.Euler(0, 80, 180);
+                    ghostTwoD.transform.rotation = _rotation;
+                    ghostTwoD.transform.position = ghostSpawnPoint[2].position;
+
+                    break;
+                case 4:
+                    _rotation = Quaternion.Euler(0, 0, 180);
+                    ghostTwoD.transform.rotation = _rotation;
+                    ghostTwoD.transform.position = ghostSpawnPoint[3].position;
+
+                    break;
+            }
+        }
     }
     protected void OnMouseDown()
     {
-
-        /*if (_count == 2)
-        {
-            ghostTwoD.transform.position = startPosition.position;
-        }*/
+        _count++;
         //random ghost next position
         randomPosition = Random.Range(1,5);
         ghostPosition(randomPosition);
-        
+        Debug.Log(_count);
     }
 }
